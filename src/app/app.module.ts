7f7b1c14acd5d8040f125a3027299af703172ef7
puNisher <lishoatwork@gmail.com>
2020-09-01 18:42:06 +0530

    The project started with SVG added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { HeaderComponent } from './header/header.component';
+import { BoardComponent } from './playground/board/board.component';
+import { ControlsComponent } from './playground/controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent,
+    HeaderComponent,
+    BoardComponent,
+    ControlsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/playground/board/board.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+
+  arr: number[] = [110, 201];
+
+  get width(){
+    return 10;
+  }
+  fillColor="blue";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get changeColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+}
