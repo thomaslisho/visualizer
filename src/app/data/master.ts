878ff4a7f838334e9ae2ac0272d1e9f876df6103
puNisher <lishoatwork@gmail.com>
2020-09-09 23:05:06 +0530

    Implemented merge sort

--- src/app/data/master.ts
+++ src/app/data/master.ts
@@ -1,5 +1,6 @@
-import { Subject } from 'rxjs';
+import { Subject, merge } from 'rxjs';
 import { ArrayElement, State } from './arrayelement';
+import { min } from 'rxjs/operators';
 
 export class Master {
   protected masterArray: ArrayElement[];
@@ -22,11 +23,79 @@ export class Master {
   }
 
   protected sort() {
-    this.selectionSort();
+    // this.heapSort();
+    // this.selectionSort();
     // this.quickSort(0, this.masterArray.length - 1);
+    this.mergeSort(0, this.masterArray.length - 1);
   }
 
+  async mergeSort(left: number, right: number) {
+    if (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      await this.mergeSort(left, mid);
+      await this.mergeSort(mid + 1, right);
+      await this.merge(left, mid, right);
+    }
+  }
 
+  async merge(left: number, mid: number, right: number) {
+    const n1 = mid - left + 1,
+      n2 = right - mid,
+      leftArray: ArrayElement[] = [],
+      rightArray: ArrayElement[] = [];
+    for (let i = 0; i < n1; i++) leftArray.push(this.masterArray[left + i]);
+    for (let j = 0; j < n2; j++) rightArray.push(this.masterArray[mid + 1 + j]);
+    let i = 0,
+      j = 0,
+      k = left;
+
+    while (i < n1 && j < n2) {
+      await this.sleep().then((_) => {
+        this.arrSubject.next(this.masterArray);
+      });
+
+      if (leftArray[i].value <= rightArray[j].value) {
+        leftArray[i].state = State.IntermediateOne;
+        this.masterArray[k] = leftArray[i];
+        await this.sleep().then((_) => {
+          this.arrSubject.next(this.masterArray);
+        });
+        i++;
+      } else {
+        rightArray[j].state = State.IntermediateTwo;
+        this.masterArray[k] = rightArray[j];
+        await this.sleep().then((_) => {
+          this.arrSubject.next(this.masterArray);
+        });
+        j++;
+      }
+      this.masterArray[k].state = State.Sorted;
+      k++;
+    }
+    while (i < n1) {
+      this.masterArray[k] = leftArray[i];
+      this.masterArray[k].state = State.IntermediateOne;
+      await this.sleep().then((_) => {
+        this.arrSubject.next(this.masterArray);
+        this.masterArray[k].state = State.Sorted;
+      });
+      i++;
+      k++;
+    }
+    while (j < n2) {
+      this.masterArray[k] = rightArray[j];
+      this.masterArray[k].state = State.IntermediateTwo;
+      await this.sleep().then((_) => {
+        this.arrSubject.next(this.masterArray);
+        this.masterArray[k].state = State.Sorted;
+      });
+      j++;
+      k++;
+    }
+    await this.sleep().then((_) => {
+      this.arrSubject.next(this.masterArray);
+    });
+  }
 
   private async swap(leftIndex: number, rightIndex: number) {
     this.masterArray[leftIndex].state = State.IntermediateTwo;
@@ -40,11 +109,15 @@ export class Master {
     this.masterArray[rightIndex].state = this.masterArray[leftIndex].state =
       State.Idle;
   }
-  
+
   private async partition(left: number, right: number) {
     var pivot = this.masterArray[Math.floor((right + left) / 2)], //middle element
       i = left, //left pointer
-      j = right; //right pointer
+      j = right;
+    pivot.state = State.Sorted;
+    await this.sleep().then((_) => {
+      this.arrSubject.next(this.masterArray);
+    });
     while (i <= j) {
       while (this.masterArray[i].value < pivot.value) {
         i++;
@@ -61,9 +134,7 @@ export class Master {
           this.arrSubject.next(this.masterArray);
           this.masterArray[j].state = State.Idle;
         });
-        // this.masterArray[j].state = State.IntermediateOne;
       }
-
       if (i <= j) {
         await this.swap(i, j);
         i++;
@@ -77,7 +148,7 @@ export class Master {
     var index: number;
     if (this.masterArray.length > 1) {
       index = await this.partition(left, right);
-      
+
       if (left < index - 1) {
         await this.quickSort(left, index - 1);
         await this.sleep().then((_) => {
@@ -94,8 +165,6 @@ export class Master {
     }
     this.masterArray[left].state = State.Sorted;
   }
-
-  
 
   private async insertionSort() {
     for (let i = 0; i < this.masterArray.length; i++) {
