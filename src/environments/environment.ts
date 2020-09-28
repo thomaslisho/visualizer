7b82a0396ad36045f0f066e94d2fe88a89c85f34
Lisho Thomas <62068885+thomaslisho@users.noreply.github.com>
2020-09-28 09:02:51 +0530

    Update environment.ts
    Updated the firebase environment IDs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDGGkpIuKISW_UGRqqsirrnJY_oQpAD4QQ',
-    authDomain: 'visualizer-7839c.firebaseapp.com',
-    databaseURL: 'https://visualizer-7839c.firebaseio.com',
-    projectId: 'visualizer-7839c',
-    storageBucket: 'visualizer-7839c.appspot.com',
-    messagingSenderId: '691741674045',
-    appId: '1:691741674045:web:345e8cdfe0a6944c70f854',
-    measurementId: 'G-P0BDTKQ7CN',
+    apiKey: '***************************',
+    authDomain: '***************************',
+    databaseURL: '*******************************',
+    projectId: '******************************',
+    storageBucket: '****************************',
+    messagingSenderId: '*************************',
+    appId: '***************************************',
+    measurementId: '*********************************',
   },
 };
 
