b96d3b4aec799f78388adee770f4c1ee21bd19cd
Lisho Thomas <62068885+thomaslisho@users.noreply.github.com>
2020-09-28 08:55:31 +0530

    Update environment.prod.ts
    Masked the prodIds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDGGkpIuKISW_UGRqqsirrnJY_oQpAD4QQ',
-    authDomain: 'visualizer-7839c.firebaseapp.com',
-    databaseURL: 'https://visualizer-7839c.firebaseio.com',
-    projectId: 'visualizer-7839c',
-    storageBucket: 'visualizer-7839c.appspot.com',
-    messagingSenderId: '691741674045',
-    appId: '1:691741674045:web:345e8cdfe0a6944c70f854',
-    measurementId: 'G-P0BDTKQ7CN',
+    apiKey: '**********************',
+    authDomain: '*************************',
+    databaseURL: '******************************',
+    projectId: '*******************',
+    storageBucket: '*************************',
+    messagingSenderId: '************************',
+    appId: '***************************************',
+    measurementId: '***********************************',
   },
 };
