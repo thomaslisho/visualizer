0b5300721cf5ca489cfd25d20c21b1644cd803ec
puNisher <lishoatwork@gmail.com>
2020-09-28 08:51:12 +0530

    Web share API

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { NgNavigatorShareService } from 'ng-navigator-share';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-header',
@@ -18,8 +20,11 @@ export class HeaderComponent implements OnInit {
     );
   constructor(
     private router: Router,
-    private breakpointObserver: BreakpointObserver
+    private breakpointObserver: BreakpointObserver,
+    private ngNavigatorShareService: NgNavigatorShareService,
+    private snackbar: MatSnackBar
   ) {}
+  title = 'Sorting Visualizer';
 
   ngOnInit(): void {}
 
@@ -28,5 +33,28 @@ export class HeaderComponent implements OnInit {
   }
   openLinkedInLisho(): void {
     window.open('https://www.linkedin.com/in/lisho-thomas/', '_blank');
+  }
+  share() {
+    if (!this.ngNavigatorShareService.canShare()) {
+      this.snackbar.open('The functionality is not supported!', 'Close', {
+        duration: 5000,
+      });
+      return;
+    }
+    this.ngNavigatorShareService
+      .share({
+        title: 'Sorting Visualizer',
+        text:
+          'A simple app visualizing sorting algorithms in an interactive environment. Do comment your thoughts and criticisms in the comment section provided!',
+        url: 'https://visualizer-7839c.web.app',
+      })
+      .then((response) => {
+        this.snackbar.open('Thank you for sharing !', 'Close', {
+          duration: 5000,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
